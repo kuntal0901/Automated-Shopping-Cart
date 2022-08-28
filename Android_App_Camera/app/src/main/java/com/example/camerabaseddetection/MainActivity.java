9c96b16f46583f6af30fedf394365eccaa8a5a79
svscsantosh <santoshsvsc2002@gmail.com>
2022-08-28 18:22:22 +0530

    New Changes

--- Android_App_Camera/app/src/main/java/com/example/camerabaseddetection/MainActivity.java
+++ Android_App_Camera/app/src/main/java/com/example/camerabaseddetection/MainActivity.java
@@ -18,8 +18,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.camerabaseddetection.ml.EfficentNetv2;
-import com.example.camerabaseddetection.ml.MobilenetV3;
-import com.example.camerabaseddetection.ml.NasnetMobile;
+import com.example.camerabaseddetection.ml.Efficentnetv2Augumented98;
+import com.example.camerabaseddetection.ml.NasnetMobileAugSan98;
 
 import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
@@ -42,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
         result = (TextView) findViewById(R.id.textView);
         imageView = (ImageView) findViewById(R.id.imageView);
         picture = (Button) findViewById(R.id.button);
-
         picture.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
@@ -63,21 +62,14 @@ public class MainActivity extends AppCompatActivity {
     public void classifyImage(Bitmap image){
         try {
             EfficentNetv2 model = EfficentNetv2.newInstance(getApplicationContext());
-            MobilenetV3 model1 = MobilenetV3.newInstance(getApplicationContext());
-            NasnetMobile model2 = NasnetMobile.newInstance(getApplicationContext());
-            // Creates inputs for reference.
+            Efficentnetv2Augumented98 model1 = Efficentnetv2Augumented98.newInstance(getApplicationContext());
+            NasnetMobileAugSan98 model2 = NasnetMobileAugSan98.newInstance(getApplicationContext());
             TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
-//            TensorBuffer inputFeature1 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
-//            TensorBuffer inputFeature2 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
 
             ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
             byteBuffer.order(ByteOrder.nativeOrder());
-
-            // get 1D array of 224 * 224 pixels in image
             int [] intValues = new int[imageSize * imageSize];
             image.getPixels(intValues, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
-
-            // iterate over pixels and extract R, G, and B values. Add to bytebuffer.
             int pixel = 0;
             for(int i = 0; i < imageSize; i++){
                 for(int j = 0; j < imageSize; j++){
@@ -89,46 +81,86 @@ public class MainActivity extends AppCompatActivity {
             }
 
             inputFeature0.loadBuffer(byteBuffer);
-//            inputFeature1.loadBuffer(byteBuffer);
-//            inputFeature2.loadBuffer(byteBuffer);
 
             // Runs model inference and gets result.
             EfficentNetv2.Outputs outputs = model.process(inputFeature0);
             TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
-            MobilenetV3.Outputs outputs1 = model1.process(inputFeature0);
+            Efficentnetv2Augumented98.Outputs outputs1 = model1.process(inputFeature0);
             TensorBuffer outputFeature1 = outputs1.getOutputFeature0AsTensorBuffer();
-            NasnetMobile.Outputs outputs2 = model2.process(inputFeature0);
+            NasnetMobileAugSan98.Outputs outputs2 = model2.process(inputFeature0);
             TensorBuffer outputFeature2 = outputs2.getOutputFeature0AsTensorBuffer();
 
             float[] data=outputFeature0.getFloatArray();
             float[] data1=outputFeature1.getFloatArray();
             float[] data2=outputFeature2.getFloatArray();
             double[] finaldata=new double[data.length];
-//            float weights[3]={0.5,0.3,0.2};
-            int i=0;
-            String [] arr={"Apple","Bean","Beetroot","Bitter_Gourd","Bottle_Gourd","Brinjal","Broccoli","Cabbage","Capsicum","Carrot","Cauliflower","Cucumber","DragonFruit","Garlic","Ginger","Guava","Kiwi","Mosambi","Muskmelon","Okra","Papaya","Pineapple","Pomegranate","Potato","Pumpkin","Radish","Sapodilla","Sweet potato","Tomato","banana","custard_apple","fig","grape","jackfruit","lemon","mango","onion","orange","pear","peas","strawberry","watermelon"};
+            double[] weights= {0.35,0.33,0.32};
+            String [] arr={"Apple","Banana","Bean","Beetroot","Bitter gourd","Bottle gourd","Brinjal","Broccoli","Cabbage","Capsicum","Carrot","Cauliflower","Cucumber","Custard apple","Dragonfruit","Fig","Garlic","Ginger","Grape","Guava","Jackfruit","Kiwi","Lemon","Mango","Mosambi","Muskmelon","Okra","Onion","Orange","Papaya","Pear","Peas","Pineapple","Pomegranate","Potato","Pumpkin","Radish","Sapodilla","Strawberry","Sweet potato","Tomato","Watermelon"};
+            String [] Model_names={"EfficientNetV2 Without Augumentation","EfficientNetV2 With Augumentation","NasNet With Augumentaion"};
+            int i;
+            double maxval;
+            maxval=-1000000.0;
+            i=0;
+            int class_model_1 = 0;
+            int class_model_2=0;
+            int class_model_3=0;
+            int final_pred=0;
             for(i=0;i<data.length;i++)
             {
-                finaldata[i]=(0.5*data[i])+(0.3*data1[i])+(0.2*data2[i]);
+                if(data[i]>maxval)
+                {
+                    maxval=data[i];
+                    class_model_1=i;
+                }
             }
+
+            maxval=-1000000.0;
             i=0;
-            double maxval=-1000000.0;
-            int classes=-1;
-            for(i=0;i<finaldata.length;i++){
+            for(i=0;i<data1.length;i++)
+            {
+                if(data1[i]>maxval)
+                {
+                    maxval=data1[i];
+                    class_model_2=i;
+                }
+            }
+
+            maxval=-1000000.0;
+            i=0;
+            for(i=0;i<data2.length;i++)
+            {
+                if(data2[i]>maxval)
+                {
+                    maxval=data2[i];
+                    class_model_3=i;
+                }
+            }
+
+
+            for(i=0;i<data.length;i++)
+            {
+                finaldata[i]=(weights[0]*data[i])+(weights[1]*data1[i])+(weights[2]*data2[i]);
+            }
+
+            maxval=-1000000.0;
+            i=0;
+            for(i=0;i<data.length;i++)
+            {
                 if(finaldata[i]>maxval)
                 {
                     maxval=finaldata[i];
-                    classes=i;
+                    final_pred=i;
                 }
             }
-            String str="Model1 "+data[0]+" Model2 "+data1[0]+" Model3 "+data2[0];
-//            result.setText("Class belonging to is "+arr[classes]);
-            result.setText(str+"Class belonging to is "+arr[classes]);
 
 
 
             // Releases model resources if no longer used.
             model.close();
+            model1.close();
+            model2.close();
+            String res=Model_names[0]+" Gives Prediction: "+arr[class_model_1]+"\n"+Model_names[1]+" Gives Prediction: "+arr[class_model_2]+"\n"+Model_names[2]+" Gives Prediction: "+arr[class_model_3]+"\n"+"Combined Model Gives Prediction: "+arr[final_pred]+"\n";
+            result.setText(res);
         } catch (IOException e) {
             // TODO Handle the exception
         }
