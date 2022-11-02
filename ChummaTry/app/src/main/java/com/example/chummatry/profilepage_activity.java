eff3bab8c78fb0e4a5148075e0abc7a961d2d41f
svscsantosh <santoshsvsc2002@gmail.com>
2022-11-02 15:01:32 +0530

    Profile page commit 1

---
+++ ChummaTry/app/src/main/java/com/example/chummatry/profilepage_activity.java
@@ -0,0 +1,84 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		profilepage
+	 *	@date 		Wednesday 02nd of November 2022 07:28:00 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class profilepage_activity extends Activity {
+
+	
+	private View _bg__profilepage_ek2;
+	private View ellipse_8;
+	private TextView svsc_santosh;
+	private TextView santoshsvsc2_gmail_com;
+	private ImageView image_removebg_preview_1;
+	private ImageView image_2;
+	private TextView previous_orders;
+	private ImageView image_prev_ui_1;
+	private TextView edit_profile;
+	private TextView logout;
+	private ImageView image_removebg_preview__2__1;
+	private ImageView image_removebg_preview__2__2;
+	private ImageView ellipse_7;
+	private View homebar_ek1;
+	private ImageView vector;
+	private ImageView vector_ek1;
+	private View ellipse_1;
+	private View ellipse_2;
+	private ImageView vector_ek2;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.profilepage);
+
+		
+		_bg__profilepage_ek2 = (View) findViewById(R.id._bg__profilepage_ek2);
+		ellipse_8 = (View) findViewById(R.id.ellipse_8);
+		svsc_santosh = (TextView) findViewById(R.id.svsc_santosh);
+		santoshsvsc2_gmail_com = (TextView) findViewById(R.id.santoshsvsc2_gmail_com);
+		image_removebg_preview_1 = (ImageView) findViewById(R.id.image_removebg_preview_1);
+		image_2 = (ImageView) findViewById(R.id.image_2);
+		previous_orders = (TextView) findViewById(R.id.previous_orders);
+		image_prev_ui_1 = (ImageView) findViewById(R.id.image_prev_ui_1);
+		edit_profile = (TextView) findViewById(R.id.edit_profile);
+		logout = (TextView) findViewById(R.id.logout);
+		image_removebg_preview__2__1 = (ImageView) findViewById(R.id.image_removebg_preview__2__1);
+		image_removebg_preview__2__2 = (ImageView) findViewById(R.id.image_removebg_preview__2__2);
+		ellipse_7 = (ImageView) findViewById(R.id.ellipse_7);
+		homebar_ek1 = (View) findViewById(R.id.homebar_ek1);
+		vector = (ImageView) findViewById(R.id.vector);
+		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+		ellipse_1 = (View) findViewById(R.id.ellipse_1);
+		ellipse_2 = (View) findViewById(R.id.ellipse_2);
+		vector_ek2 = (ImageView) findViewById(R.id.vector_ek2);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
