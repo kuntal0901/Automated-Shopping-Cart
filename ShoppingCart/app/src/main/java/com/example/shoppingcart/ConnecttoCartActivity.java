110b7cf69f7345d35c1e93681be6d09a6b4e984f
kuntal0901 <54110827+kuntal0901@users.noreply.github.com>
2022-11-02 15:25:31 +0530

    Changes

--- ShoppingCart/app/src/main/java/com/example/shoppingcart/ConnecttoCartActivity.java
+++ ShoppingCart/app/src/main/java/com/example/shoppingcart/ConnecttoCartActivity.java
@@ -33,7 +33,8 @@ public class ConnecttoCartActivity extends AppCompatActivity {
     private EditText cartname;
     TextView status;
     Button connect;
-    public boolean connected = true;
+    public boolean connected = false
+            ;
     ProgressDialog progressDialog;
     BluetoothAdapter mBluetoothAdapter;
     BluetoothSocket mmSocket;
