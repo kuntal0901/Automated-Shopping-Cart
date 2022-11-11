3668fda514dba6c6bc94edac916aee0374c0ff94
vijay-murugan <56683057+vijay-murugan@users.noreply.github.com>
2022-11-11 14:23:48 +0530

    payment page setp1

--- ShoppingCart_1/app/src/main/java/com/example/shoppingcart/PaymentActivity.java
+++ ShoppingCart_1/app/src/main/java/com/example/shoppingcart/PaymentActivity.java
@@ -1,5 +1,8 @@
 package com.example.shoppingcart;
 
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +11,14 @@ public class PaymentActivity extends AppCompatActivity {
 
     Button yesButton;
     Button noButton;
-//    @Override
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);//    @Override
+        setContentView(R.layout.activity_payment);
+        ProgressDialog pg = new ProgressDialog(this);
+        yesButton = (Button) findViewById(R.id.yes_Button);
+        noButton = (Button) findViewById(R.id.no_Button);
 //    protected void
+    }
 }
